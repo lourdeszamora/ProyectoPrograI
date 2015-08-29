129165cdf951ae512b4b7a438a1efbdf34f33286
korrai <francisavp@hotmail.com>
2015-08-29 16:26:07 -0600

    Proyecto

---
+++ Francis1/variables.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+/**
+ *
+ * @author Francis
+ */
+public class variables {
+    public static String[] usuarios = new String[19], contraseñas = new String[19];
+    public static String userLogin = " ", userLogin2 = " ";
+    public static int[] puntos = new int[19]; 
+    public static int i = 0, cantGhosts = 8;
+    public CreateUser createu = new CreateUser();
+    public MenuInicial menui = new MenuInicial();
+    public Login log = new Login();
+    
+}
