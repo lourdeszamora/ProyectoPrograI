52902af7275d1f64a01228e1c6941866c467ce52
DeliaRivera <delia-0095@hotmail.com>
2015-08-30 10:13:09 -0600

    proyecto

---
+++ Ghost/src/ghost/variables.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ghost;
+
+/**
+ *
+ * @author Delia
+ */
+public class variables {
+    public static String[] usuarios = new String[19], contraseñas = new String[19];
+    public static int i = 0;
+    public static String userLogin = " ";
+}
